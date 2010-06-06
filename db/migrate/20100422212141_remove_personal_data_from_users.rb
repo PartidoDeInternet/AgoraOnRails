ebe87bccc85aa708dcbbcf2b232f7b7c71b33bb8
rgarcia <voodoorai2000@gmail.com>
2010-06-06 11:27:27 -0400

    smooth running first time migrations

--- db/migrate/20100422212141_remove_personal_data_from_users.rb
+++ db/migrate/20100422212141_remove_personal_data_from_users.rb
@@ -1,6 +1,5 @@
 class RemovePersonalDataFromUsers < ActiveRecord::Migration
   def self.up
-    rename_column :users, :dni, :login
     remove_column :users, :first_name
     remove_column :users, :last_name
     Vote.delete_all
@@ -10,6 +9,5 @@ class RemovePersonalDataFromUsers < ActiveRecord::Migration
   def self.down
     add_column :users, :last_name, :string,                         :null => false
     add_column :users, :first_name, :string,                        :null => false
-    rename_column :users, :login, :dni
   end
 end
