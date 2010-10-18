89ac82238ae5e96a11d53a05dfaaa41474b94fd6
rgarcia <voodoorai2000@gmail.com>
2010-10-18 14:38:03 +0200

    bundler setup

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source "http://rubygems.org"
+
+gem "rails", "2.3.5"
+gem "progressbar"
+gem "mechanize"
+gem "authlogic", :require => "authlogic"
+gem "inherited_resources", "1.0.6"
+
+group :test do
+  gem "rspec", "1.3.0"
+  gem "rspec-rails", "1.3.2"
+  gem "capybara" 
+  gem 'steak'
+  gem 'webmock'
+end
+
+group :development do
+  gem 'sqlite3-ruby'
+end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,8 +26,3 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
-
-config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
-config.gem 'steak', :lib => false
-config.gem 'capybara', :lib => false
-config.gem 'webmock', :lib => false
