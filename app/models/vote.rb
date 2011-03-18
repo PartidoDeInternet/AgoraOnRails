5a5972cf69004a35bc6268a20b313d55b31e5475
Juanjo Bazán <jjbazan@gmail.com>
2011-03-18 23:23:27 +0100

    adding possibility to mark the vote as confidential when voting a proposal

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,8 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   
   scope :order_by_represented_users, order("users.represented_users_count DESC").includes("user")
+  scope :public, where("votes.confidential = ?",false)
+  scope :confidential, where("votes.confidential = ?",true)
   
   validates_uniqueness_of :proposal_id, :scope => :user_id
   
@@ -19,6 +21,14 @@ class Vote < ActiveRecord::Base
   
   def self.choices
     ["in_favor", "against", "abstention"]
+  end
+  
+  def public?
+    !confidential
+  end
+  
+  def confidential?
+    confidential
   end
 
 end
