b9855bd14051f9d92dd92d468a09ab60d9818504
Jorge <jorge@mrdias.com>
2011-03-20 20:10:58 +0100

    Using new APIs for FB and Twitter Proposals behave like open graph pages

---
+++ app/helpers/share_helper.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+module ShareHelper
+  def image_url(source)
+    abs_path = image_path(source)
+    unless abs_path =~ /^http/
+      abs_path = "#{request.protocol}#{request.host_with_port}#{abs_path}"
+    end
+   abs_path
+  end
+
+  def share_text(proposal)
+    "He votado a través del partido del internet la propuesta #{proposal_url(proposal)}"
+  end
+
+  def share_on_facebook_link(proposal)
+    %{<iframe src="http://www.facebook.com/plugins/like.php?href=#{CGI.escape(proposal_url(proposal))}&locale=es_ES&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=recommend&amp;font=arial&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true" name="fb-share"></iframe>
+}.html_safe
+  end
+
+  def share_on_twitter_link(proposal)
+    %{<iframe allowtransparency="true" frameborder="0" scrolling="no"
+        src="http://platform.twitter.com/widgets/tweet_button.html?url=#{CGI.escape(proposal_url(proposal))}&via=#{CGI.escape("pdi: El Partido de Internet")}&text=#{CGI.escape(share_text(proposal))}&lang=es&count=horizontal"
+        style="width:130px; height:50px;" name="twitter-share"></iframe>
+}.html_safe
+  end
+end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -9,23 +9,4 @@ module VotesHelper
     end
   end
 
-  def share_text(proposal)
-    "He votado a través del partido del internet la propuesta #{proposal_url(proposal)}"
-  end
-
-  def share_on_facebook_link(proposal)
-    content_tag(:a, "Compartir en Facebook", :class => "share_facebook", :name => "fb_share",
-                :href => "http://www.facebook.com/sharer.php?u=#{CGI.escape(proposal_url(proposal))}&t=#{CGI.escape(share_text(proposal))}",
-                :target => "_blank")
-  end
-
-  def share_on_twitter_link(proposal)
-    title = "Compartir en Twitter"
-    content_tag(:a, title, :id => "share_twitter",
-                :href => "http://twitter.com/home?status=#{share_text(proposal)}",
-                :data => proposal_url(proposal),
-                :title => title,
-                :target => "_blank")
-  end
-
 end
