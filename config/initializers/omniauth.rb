78105b9ed186eaf5bddd8b18ec2960de3f126ce6
rgarcia <voodoorai2000@gmail.com>
2013-06-02 14:57:58 +0900

    twitter authentication in popup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,11 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   if ENV['TWITTER_CONSUMER_KEY'].blank? or ENV['TWITTER_CONSUMER_SECRET'].blank?
     warn "*" * 80
-    warn "WARNING: Missing consumer key or secret. First, register an app with Twitter at"
-    warn "https://dev.twitter.com/apps to obtain OAuth credentials. Then, start the server"
-    warn "with the command: TWITTER_CONSUMER_KEY=abc TWITTER_CONSUMER_SECRET=123 rails server"
+    warn "WARNING: Missing consumer key or secret."
     warn "*" * 80
   end
-  use OmniAuth::Strategies::Twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -74,4 +74,16 @@ $(function(){
   }
 
   $(".overlayfield").overlayfield();
+
+  // twitter authentication in popup
+  function popupCenter(url, width, height, name) {
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
+    return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+  }
+
+  $("a.popup").click(function(e) {
+    popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
+    e.stopPropagation(); return false;
+  });
 });
