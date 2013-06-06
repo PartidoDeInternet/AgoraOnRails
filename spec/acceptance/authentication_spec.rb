405f42a945bb385236a97ab28c32c21030de2116
rgarcia <voodoorai2000@gmail.com>
2013-06-06 15:20:14 +0900

    specs to sign up and forgot password

--- spec/acceptance/authentication_spec.rb
+++ spec/acceptance/authentication_spec.rb
@@ -4,9 +4,32 @@ require File.dirname(__FILE__) + '/acceptance_helper'
 feature "Authentication", %q{
   In order to keep track of what proposals I have voted
   As a citizen
-  I want to sign in and out of the application
+  I want to basic authentication funcionalities
 } do
 
+  scenario "Sign up with standard account" do
+    visit "/users/sign_in"
+    within("#signup") do
+      fill_in :user_name, with: "Punset"
+      fill_in :user_email, with: "punset@spain.com"
+      fill_in :user_password, with: "secret"
+      click_button "Sign up"
+    end
+    page.should have_content "Estoy logueado como Punset"
+  end
+
+  scenario "Sign in with standard account", :js do
+    user = create_user 
+    visit "/users/sign_in"
+    click_link "Entra con tu cuenta de Agoraonrails"
+    within("#agoraonrails-account") do
+      fill_in :user_email, with: user.email
+      fill_in :user_password, with: user.password
+      click_button "Sign in"
+    end
+    page.should have_content "Estoy logueado como Mother Fucking Real User"
+  end
+  
   scenario "Sign in with twitter" do
     user = create_user name: "José Luis Sampedro"
     OmniAuth.config.test_mode = true
@@ -42,5 +65,19 @@ feature "Authentication", %q{
     click_link "quiero salir de la applicación"
     page.should have_content "Cerraste sesión correctamente"
     page.should_not have_content "Estoy logueado como Mother Fucking Real User"
+  end
+  
+  scenario "Forgot password for standard account", :js do
+    create_user email: "punset@spain.com"
+    
+    visit "/users/sign_in"
+    click_link "Entra con tu cuenta de Agoraonrails"
+    click_link "¿Olvidaste tu contraseña?"
+    
+    within("#forgot-password") do
+      fill_in :user_email, with: "punset@spain.com"
+      click_button "Restablecer contraseña"
+    end
+    page.should have_content "Recibirás un email con instrucciones para reiniciar tu contraseña en unos minutos."
   end
 end
