1fd1b5faa1b475aed2675fea3f15bd4c124e1b22
Miguel Angel Davila <miguelangel@fkla.org>
2013-10-13 16:10:09 +0200

    menu link helper to include icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,11 @@ module ApplicationHelper
     action_name == "new" and ["votes", "spokesmen"].include?(controller_name)
   end
   
-  def menu_link(text, url, icon)
+  def menu_link(text, url, icon='')
     content_tag :li do
-      link_to text, url, :class => "nav-link #{icon}"
+      link_to url, :class => "navlinks" do
+        "<span class='#{icon}'></span>#{text}".html_safe
+      end
     end
   end
 
