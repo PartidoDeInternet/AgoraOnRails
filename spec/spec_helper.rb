07b232edf65cc6a86151205bb001369d2a054d9f
rgarcia <voodoorai2000@gmail.com>
2014-01-23 03:35:54 +0100

    checking for :focus option when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@ RSpec.configure do |config|
   config.include WebMock::API
   config.use_transactional_fixtures = false
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   
   config.before(:each) do
